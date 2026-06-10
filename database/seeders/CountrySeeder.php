4ecf864758754c8675380262f43ea503e7b7a827
elmerrondon <elmerrondonj22@gmail.com>
2026-06-10 15:17:24 -0400

    feat: seeders country and state

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Locations\Country;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $countries = [
+            ["name" => "Venezuela"],
+            ["name" => "Colombia"],
+            ["name" => "Estados Unidos"],
+            ["name" => "Chile"],
+            ["name" => "Argentina"],
+            ["name" => "España"],
+            ["name" => "Brasil"],
+            ["name" => "Mexico"]
+            ];
+
+            foreach($countries as $country){
+                Country::firstOrCreate($country);
+            }
+    }
+}
---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Locations\Country;
+use App\Models\Locations\State;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $venezuela = Country::where("name", "Venezuela")->firts();
+        $colombia = Country::where("name", "Colombia")->firts();
+        $usa = Country::where("name", "Estados Unidos")->firts();
+
+        $states = [
+            ["name" => "Tachira", "country_id" => $venezuela->id],
+            ["name" => "Merida", "country_id" => $venezuela->id],
+            ["name" => "Trujillo", "country_id" => $venezuela->id],
+            ["name" => "Zulia", "country_id" => $venezuela->id],
+            ["name" => "Lara", "country_id" => $venezuela->id],
+            ["name" => "Carabobo", "country_id" => $venezuela->id],
+            ["name" => "Distrito Capital", "country_id" => $venezuela->id],
+            ["name" => "Falcon", "country_id" => $venezuela->id],
+            ["name" => "Santander", "country_id" => $colombia->id],
+            ["name" => "Norte de Santander", "country_id" => $colombia->id],
+            ["name" => "Antioquia", "country_id" => $colombia->id],
+            ["name" => "Caldas", "country_id" => $colombia->id],
+            ["name" => "Cundinamarca", "country_id" => $colombia->id],
+            ["name" => "Florida", "country_id" => $usa->id],
+            ["name" => "California", "country_id" => $usa->id],
+            ["name" => "Texas", "country_id" => $usa->id],
+            ["name" => "Nuevo Mexico", "country_id" => $usa->id],
+            ["name" => "Arizona", "country_id" => $usa->id]          
+        ];
+
+        foreach($states as $state){
+            State::firstOrCreate($state);
+        }
+    }
+}
