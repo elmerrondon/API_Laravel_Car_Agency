68d0d5a51237880fee0a29936899dc6ef3705f02
elmerrondon <elmerrondonj22@gmail.com>
2026-06-23 17:06:45 -0400

    feat: CarTypes EndPoints

---
+++ app/Http/Controllers/Cars/CarTypeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Cars;
+
+use App\DTOs\Cars\CarTypeData;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Cars\CarType\StoreCarTypeRequest;
+use App\Http\Requests\Cars\CarType\UpdateCarTypeRequest;
+use App\Http\Resources\Cars\CarTypeResource;
+use App\Models\Cars\CarType;
+use App\Services\Cars\CarTypeService;
+use Illuminate\Http\Request;
+
+class CarTypeController extends Controller
+{
+    public function index(CarTypeService $service){
+        $carTypes = $service->getAllPaginated();
+
+        return CarTypeResource::collection($carTypes);
+    }
+
+    public function show(CarType $carType){
+        return new CarTypeResource($carType);
+    }
+
+    public function store(StoreCarTypeRequest $request, CarTypeService $service){
+        $dto = CarTypeData::fromRequest($request);
+
+        $carType = $service->create($dto);
+
+        return new CarTypeResource($carType);
+    }
+
+    public function update(CarType $carType, UpdateCarTypeRequest $request, CarTypeService $service){
+        $dto = CarTypeData::fromRequest($request);
+
+        $carType = $service->update($carType, $dto);
+
+        return new CarTypeResource($carType);
+    }
+
+    public function destroy(CarType $carType, CarTypeService $service){
+        $service->delete($carType);
+
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Cars\CarTypeController;
 use App\Http\Controllers\Locations\BranchController;
 use App\Http\Controllers\Locations\CityController;
 use App\Http\Controllers\Locations\CountryController;
@@ -11,3 +12,6 @@ Route::apiResource('countries', CountryController::class);
 Route::apiResource('states', StateController::class);
 Route::apiResource('cities', CityController::class);
 Route::apiResource('branches', BranchController::class);
+
+// Cars
+Route::apiResource('car-types', CarTypeController::class);
